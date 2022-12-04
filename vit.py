235c0cad8022ed9f287f2586524c10e42439ad88
Gonzalo Hernandez <gonzalo.hernandez@ug.uchile.cl>
2022-12-03 22:12:35 -0300

    Se implementa ViT

--- vit.py
+++ vit.py
@@ -1,2 +1,17 @@
-from utils.attention import Atention
+import torch
+from torch import nn
+from utils.encoder import Encoder
 
+class ViT(nn.Module):
+    def __init__(self, dim, num_heads, dim_heads, num_patches, n_classes):
+        super().__init__()
+
+        self.position_emb = nn.Parameter(torch.rand(1, num_patches + 1, dim))
+        self.encoder = Encoder(dim, num_heads, dim_heads)
+
+        self.fc_out = nn.Linear(dim, n_classes)
+
+    def forward(self, img):
+        x = self.encoder(img)
+        out = self.fc_out(x)
+        return out
