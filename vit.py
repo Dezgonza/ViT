3e2a809a2ed7dcfc07e1ccff5a5375802c308f5a
Gonzalo Hernandez <stkierke@gmail.com>
2022-12-09 18:18:55 +0100

    Se complementa modelo vit

--- vit.py
+++ vit.py
@@ -3,15 +3,22 @@ from torch import nn
 from utils.model.encoder import Encoder
 
 class ViT(nn.Module):
-    def __init__(self, dim, num_heads, dim_heads, num_patches, n_classes):
+    def __init__(self, n_layer, n_classes, dim, num_heads, dim_heads, num_patches, dropout):
         super().__init__()
 
-        self.position_emb = nn.Parameter(torch.rand(1, num_patches + 1, dim))
-        self.encoder = Encoder(dim, num_heads, dim_heads)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+        self.dropout = nn.Dropout(dropout)
+        self.pos_emb = nn.Parameter(torch.rand(1, num_patches + 1, dim))
+        self.encoder = Encoder(n_layer, dim, num_heads, dim_heads)
         self.fc_out = nn.Linear(dim, n_classes)
+        self.norm = nn.LayerNorm(dim)
 
     def forward(self, img):
-        x = self.encoder(img)
+        x = torch.cat([self.cls_token, img], dim = 1)
+        x += self.pos_emb[:, : (n + 1)]
+        x = self.dropout(x)
+        x = self.encoder(x)[:, 0]
+        x = self.norm(x)
         out = self.fc_out(x)
 
         return out
