edd2a8b366d6c1ed94119a84495dccec4bae1887
Gonzalo Hernandez <stkierke@gmail.com>
2022-12-09 13:39:51 +0100

    Se agrega red lineal post-encoder

---
+++ utils/linear.py
@@ -0,0 +1,13 @@
+from torch import nn
+
+class LinearNet(nn.Module):
+    def __init__(self, init_dim, inner_dim, dropout):
+        super().__init__()
+        self.fn_init = nn.Linear(init_dim, inner_dim)
+        self.fn_out = nn.Linear(inner_dim, inner_dim)
+        self.drop = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = self.drop(nn.GeLU(self.fn_init(x)))
+        out = self.drop(self.fn_out(x))
+        return out
