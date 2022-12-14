366f94d1ffb380edabb6b5396357239a959b2d56
Gonzalo Hernandez <stkierke@gmail.com>
2022-12-14 03:12:25 +0100

    Fix

--- model/attention.py
+++ model/attention.py
@@ -17,7 +17,7 @@ class Attention(nn.Module):
         
     def forward(self, img):
         qkv = self.split_to_qkv(img).chunk(3, dim = -1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.num_heads), qkv)
         #qkv = qkv.reshape(img.size()[0], img.size()[1], self.num_heads, 3 * self.dim_heads)
         #qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
         #q, k, v = qkv.chunk(3, dim=-1)
