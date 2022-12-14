65fe485a40befe4c897dce05c1712865889017fa
Gonzalo Hernandez <stkierke@gmail.com>
2022-12-14 03:37:53 +0100

    Fix

--- model/linear.py
+++ model/linear.py
@@ -5,7 +5,7 @@ class LinearNet(nn.Module):
     def __init__(self, init_dim, inner_dim, dropout):
         super().__init__()
         self.fn_init = nn.Linear(init_dim, inner_dim)
-        self.fn_out = nn.Linear(inner_dim, inner_dim)
+        self.fn_out = nn.Linear(inner_dim, init_dim)
         self.drop = nn.Dropout(dropout)
 
     def forward(self, x):
