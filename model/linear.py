7ff4ae820778dfffd006b63560f2a3fc54d84984
Gonzalo Hernandez <stkierke@gmail.com>
2022-12-14 03:24:57 +0100

    Fix

--- model/linear.py
+++ model/linear.py
@@ -3,12 +3,13 @@ from torch import nn
 class LinearNet(nn.Module):
     def __init__(self, init_dim, inner_dim, dropout):
         super().__init__()
+        self.gelu = nn.GeLU()
         self.fn_init = nn.Linear(init_dim, inner_dim)
         self.fn_out = nn.Linear(inner_dim, inner_dim)
         self.drop = nn.Dropout(dropout)
 
     def forward(self, x):
-        out = self.drop(nn.ReLU(self.fn_init(x)))
+        out = self.drop(self.gelu(self.fn_init(x)))
         out = self.drop(self.fn_out(x))
         
         return out
